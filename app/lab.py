ec35b2b05dce6afdc232dcac0769d20e26adfeb5
yolenlew <yolenlew@163.com>
2025-12-21 23:44:36 +0800

    feat: flask-async异步

--- app/lab.py
+++ app/lab.py
@@ -2,9 +2,17 @@
 # @Time : 2025/5/25
 # @Author : Yolen
 # -----------------------------------------------
-from flask import Flask
+import asyncio
+
+from flask import Flask, current_app
 
 
 def create_app():
     app = Flask(__name__)
+
+    @app.before_request
+    def before_first_request():
+        loop = asyncio.get_event_loop()
+        current_app.loop = loop
+
     return app
--- main.py
+++ main.py
@@ -2,11 +2,29 @@
 # @Time : 2025/5/25
 # @Author : Yolen
 # -----------------------------------------------
+import asyncio
+
+from hypercorn import Config
+from hypercorn.asyncio import serve
+
 from app.lab import create_app
 
-APP = create_app()
+app = create_app()
+
+@app.route("/async")
+async def async_route():
+    await asyncio.sleep(2)
+    return {"Hello": "World"}
+
+@app.route("/sync")
+def sync_route():
+    return {"Hello": "World"}
 
 # 官方原文：
 # 请务必把 app.run() 放在 if __name__ == '__main__': 内部或者放在单独 的文件中，这样可以保证它不会被调用。
 if __name__ == '__main__':
-    APP.run()
+    config = Config()
+    config.bind = ["127.0.0.1:8086"]
+    config.workers = 1  # 关键：强制单Worker
+    asyncio.run(serve(app, config))
+    # app.run(host='127.0.0.1', port=8086)
